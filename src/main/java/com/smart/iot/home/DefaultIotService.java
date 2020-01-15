50396a94147d0ea0144cfc1e06ab139bae2b8449
Artem Kudria <ak98nneon@gmail.com>
2020-01-15 21:17:28 +0200

    Updated sneaky trow construction.

--- src/main/java/com/smart/iot/home/DefaultIotService.java
+++ src/main/java/com/smart/iot/home/DefaultIotService.java
@@ -1,11 +1,10 @@
 package com.smart.iot.home;
 
-import com.google.zxing.WriterException;
-import com.smart.iot.SmartIotException;
+import static com.smart.iot.supply.SneakyTrow.sneaky;
+
 import com.smart.iot.home.entity.Fridge;
 import com.smart.iot.home.entity.Fridge.Builder;
 import com.smart.iot.qr.QrCodeGenerator;
-import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
@@ -29,11 +28,7 @@ public class DefaultIotService implements IotService {
     String link = "http://localhost:8080/iot/fridges/" + id;
     Fridge fridge = new Builder().with(x -> {
       x.id(id);
-      try {
-        x.link(qrCodeGenerator.generateQRCodeImage(link));
-      } catch (WriterException | IOException e) {
-        throw new SmartIotException(e.getMessage());
-      }
+      x.link(sneaky(() -> qrCodeGenerator.generateQRCodeImage(link)));
       x.productList(Collections.emptyList());
     }).createFridge();
     return fridgeRepository.save(fridge);
--- src/main/java/com/smart/iot/supply/SneakyTrow.java
+++ src/main/java/com/smart/iot/supply/SneakyTrow.java
@@ -1,21 +1,26 @@
-//package com.smart.iot.supply;
-//
-//import com.smart.iot.SmartIotException;
-//import java.util.Objects;
-//import java.util.function.Consumer;
-//import java.util.function.Supplier;
-//
-//public class SneakyTrow {
-//
-//  public static <T> Supplier<T> sneaky(Consumer<T> consumer) {
-//    Objects.requireNonNull(consumer);
-//    return x -> {
-//      try {
-//        consumer.accept(x);
-//      } catch (Exception e) {
-//        throw new SmartIotException(e.getMessage());
-//      }
-//    };
-//    return null;
-//  }
-//}
+package com.smart.iot.supply;
+
+import java.util.Objects;
+import java.util.concurrent.Callable;
+
+public final class SneakyTrow {
+
+  private SneakyTrow() {
+    throw new UnsupportedOperationException();
+  }
+
+  public static <T> T sneaky(Callable<? extends T> callable) {
+    Objects.requireNonNull(callable);
+    try {
+      return callable.call();
+    } catch (Exception e) {
+      sneakyTrow(e);
+    }
+    throw new AssertionError();
+  }
+
+  @SuppressWarnings("unchecked")
+  private static <T extends Exception> void sneakyTrow(Exception e) throws T {
+    throw (T) e;
+  }
+}
