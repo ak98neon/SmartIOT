195e8e23b3abe46534e7e6ba0b282ea06f516155
Artem Kudria <ak98nneon@gmail.com>
2020-01-20 00:20:47 +0200

    Updated signup method

--- src/main/java/com/smart/iot/web/security/AuthController.java
+++ src/main/java/com/smart/iot/web/security/AuthController.java
@@ -3,6 +3,7 @@ package com.smart.iot.web.security;
 import com.smart.iot.kit.UserService;
 import com.smart.iot.web.dto.UserSignUpDto;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,14 +18,24 @@ public class AuthController {
     this.userService = userService;
   }
 
+  @GetMapping("/login")
+  public String logInPage() {
+    return "auth/logIn";
+  }
+
   @PostMapping("/login")
   public String logIn() {
-    return "auth/logIn";
+    return "redirect:/iot/";
+  }
+
+  @GetMapping("/signup")
+  public String signUpPage() {
+    return "auth/signUp";
   }
 
   @PostMapping("/signup")
   public String signUp(@RequestBody UserSignUpDto userSignUpDto) {
     userService.createUser(userSignUpDto.toUser());
-    return "auth/signUp";
+    return "redirect:/iot/";
   }
 }
--- src/main/java/com/smart/iot/web/security/WebSecurityConfiguration.java
+++ src/main/java/com/smart/iot/web/security/WebSecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -27,13 +28,20 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     http.cors().and()
         .csrf().disable()
         .authorizeRequests()
-        .antMatchers("/api/public").permitAll()
+        .antMatchers("/iot/", "/auth/**", "/resources/**").permitAll()
         .anyRequest().authenticated()
         .and()
         .addFilter(new JwtAuthenticationFilter(authenticationManager(), secret))
         .addFilter(new JwtAuthorizationFilter(authenticationManager(), secret))
         .sessionManagement()
         .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+  }
+
+  @Override
+  public void configure(WebSecurity web) throws Exception {
+    web
+        .ignoring()
+        .antMatchers("/resources/**");
   }
 
   @Bean
