e23ce9bfd714451a52472d4f20f030ba944bc48b
ak98neon <artem_kudria@epam.com>
2020-01-21 00:54:41 +0200

    Updated scripts for run local dev service in docker compose.

--- env/dev-env-down.sh
+++ env/dev-env-down.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
+docker rm -f arkudrya/smart-iot
 docker-compose -f docker-compose.service.yml -p iot-env stop
--- env/dev-env-up.sh
+++ env/dev-env-up.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
+docker build -t arkudrya/smart-iot ../
+docker tag arkudrya/smart-iot arkudrya/smart-iot:latest
 docker-compose -f docker-compose.service.yml -p iot-env up -d
