732e2c10dd4ce00620ae111fb2e1b22d437fbc96
Artem Kudria <ak98nneon@gmail.com>
2020-01-21 21:08:00 +0200

    Added maven command in local script

--- env/dev-env-up.sh
+++ env/dev-env-up.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+mvn clean && mvn install
 docker build -t arkudrya/smart-iot ../
 docker tag arkudrya/smart-iot arkudrya/smart-iot:latest
 docker-compose -f docker-compose.service.yml -p iot-env up -d
